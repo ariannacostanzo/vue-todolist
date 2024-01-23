567cff14fbd14ca883650269ca3671a9cd7c95f8
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-23 14:27:19 +0100

    scaffolding

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// console.log(Vue)
+
+const {createApp} = Vue;
+
+const app = createApp ({
+    data() {
+        return {
+            tasks,
+            
+        }
+    }
+
+
+});
+
+app.mount('#root')
