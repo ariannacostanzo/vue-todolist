dbfb21ca7bd5345258b98813d5013c5ece7b5ae0
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-23 15:57:36 +0100

    completing bonus 2

--- js/main.js
+++ js/main.js
@@ -15,22 +15,19 @@ const app = createApp ({
             this.tasks.forEach((task, i) => {if (id === task.id) this.tasks.splice(i, 1)}) 
         },
         addTask() {
-
             if (this.newTaskText) {
                 const newTask = {
                     id: new Date().toISOString(),
                     done: false,
                     text: this.newTaskText
                 }
-    
                 this.tasks.push(newTask)
                 this.newTaskText = ''
-
-            } else {
-                return
-            }
-
-            
+            } 
+            return 
+        },
+        setDoneTask(id) {
+            this.tasks.forEach((task) => {if (id === task.id) task.done = !task.done}) 
         }
     }
 
